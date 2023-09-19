5c3d1cf826e4efb04f7b3fa1bb1a745ebafb61a3
RENZO SARMIENTO <renzo.sarmiento@tecsup.edu.pe>
2023-09-19 17:23:43 -0500

    add changes in StudentController

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -1,5 +1,23 @@
 package com.tecsup.lab03.controller;
 
+import com.tecsup.lab03.model.Student;
+
 public class StudentController {
 
+    public void createStudent(String name, int age) {
+        Student student = new Student(name, age);
+        // Lógica para guardar el estudiante en la base de datos o realizar alguna acción
+    }
+
+    public void updateStudent(int id, String newName, int newAge) {
+        // Lógica para actualizar un estudiante por su ID
+    }
+
+    public void deleteStudent(int id) {
+        // Lógica para eliminar un estudiante por su ID
+    }
+
+    public void listStudents() {
+        // Lógica para listar todos los estudiantes
+    }
 }
